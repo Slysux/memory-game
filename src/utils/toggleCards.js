3a7d41fc8e79b482e63cadbae04c8dac78ae3f7a
Slysux <cajita2002@outlook.es>
2021-10-15 18:12:01 -0500

    feat: Wrapper styles. Change card styles. fix: ToggleCards function

--- src/utils/toggleCards.js
+++ src/utils/toggleCards.js
@@ -9,11 +9,12 @@ const toggleCards = async (e) => {
     if (e.target.localName === 'div' && cardsSelected.length < 2) {
         frontArr.forEach((el, i) => {
             if (el === cardTarget) {
-                cardTarget.innerHTML = `<img loading="lazy" src="${data[i].url}" alt="${data[i].breeds.length !== 0 ? (data[i].breeds[0].name + ' ') : ''}Cat image">`
+                const alt = data[i].breeds.length !== 0 ? (data[i].breeds[0].name + ' ') : '';
+                cardTarget.innerHTML = `<img loading="lazy" src="${data[i].url}" alt="${alt}Cat image">`
                 cardsSelected.push(cardTarget)
+                e.target.parentElement.classList.toggle('card--active')
             }
         })
-        e.target.parentElement.classList.toggle('card--active')
     } 
     
     if (cardsSelected.length === 2) {
